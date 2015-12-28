e7466fef27c6feaa5c9d3150fe5ee5c5fb19f7a7
Martin Swift <martin@swift.is>
2015-12-28 00:35:20 +0000

    Stada breytt í Starfsstod.

--- vaktir/admin.py
+++ vaktir/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from vaktir.models import Timabil,Stada,Tegund,Vakt,Felagi,Skraning
+from vaktir.models import Timabil,Starfsstod,Tegund,Vakt,Felagi,Skraning
 
 admin.site.register(Timabil)
-admin.site.register(Stada)
+admin.site.register(Starfsstod)
 admin.site.register(Tegund)
 admin.site.register(Vakt)
 admin.site.register(Felagi)
