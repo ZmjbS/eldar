93ed5a6d5af63a6db81394383b968c594f490bae
Martin Swift <martin@swift.is>
2016-10-31 13:57:02 +0000

    Bætt við dállkum (sumum breytanlegum) fyrir Vörur í admin-viðmótinu.

--- lager/admin.py
+++ lager/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from lager.models import Vara, Verd
 
-admin.site.register(Vara)
+class VaraAdmin(admin.ModelAdmin):
+	list_display = ('argerd', 'fulltnafn', 'stuttnafn', 'stada', 'nytt', )
+	list_editable = ('stuttnafn', 'stada')
+
+admin.site.register(Vara, VaraAdmin)
 admin.site.register(Verd)
