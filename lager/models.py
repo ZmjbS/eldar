bb4805f91a049dde03b42d653e5dfd524e07f0de
Martin Swift <martin@swift.is>
2016-10-31 13:57:33 +0000

    Breytingar á lager-módúlnum.

--- lager/models.py
+++ lager/models.py
@@ -1,55 +1,79 @@
 from django.db import models
 
-# TODO: er þetta réttur syntax?
-from eldar.vaktir import Starfsstod
+from vaktir.models import Starfsstod
 
 import math
 
 class Vara(models.Model):
+	'''
+	Hver vara hefur sér verð sem er geymt í sér hlut til að geta safnað upp
+	verðsögu vörunnar.
+
+	Vörur hafa ákveðna stöðu en ennfremur er hægt að tengja einingar við vöruna
+	til að tilgreina staðsetningu þeirra til að auðvelda vörutalningu.
+	'''
+
 	# Vörunúmer er sett saman úr þremur númerum:
 	#   aavvrr
-	# þar sem aa er árgerðin (t.d. 08 fyrir 2008), vv er vörugerðin (sjá neðar) og rr er raðnúmer þess skotelds þeirrar gerðar þess árs.
+	# þar sem aa er árgerðin (t.d. 08 fyrir 2008), vv er vörugerðin
+	# (sjá neðar) og rr er raðnúmer þess skotelds þeirrar gerðar
+	# þess árs.
 	argerd = models.SmallIntegerField()
 	vorugerd = models.SmallIntegerField()
 	radnumer = models.SmallIntegerField()
 
+	# Kassanúmerið er vörunúmer framleiðanda.
+	kassanumer = models.CharField(max_length=8, blank=True)
+
 	# Stutt nafn skotelds er eitthvað sem passar auðveldar á verðmiða.
 	fulltnafn = models.CharField(max_length=64)
-	stuttnafn = models.CharField(max_length=64)
-
-	# Höfum það heldur þannig að hver vara geti haft mörg verð (og svo notum við bara það nýjasta frá hverju ári. Hendum hins vegar ekki hinum heldur búum við bara til verð og tengjum það vöru með ForeignKey
-	## Verð vörunnar.
-	#verd = models.SmallIntegerField()
+	stuttnafn = models.CharField(max_length=64, blank=True)
 
 	# Staða vörunnar eins og er.
+	# TODO: Kannski bara sleppa þessu í stað Eining-class-ans og fallsins
+	# að neðan.
+	# Eða nota hvort tveggja ... Þessi staða er þá fyrir smærri einingar
+	# sem en hitt fyrir óopnaða kassa ... eða þá að þetta sé fyrir smádót
+	# en Eining fyrir stærri einingar sem mögulega virkjast með því að
+	# setja þetta sem 0, -1, eða 99999.
+	stada = models.SmallIntegerField(null=True, blank=True)
 
-	# TODO: Kannski bara sleppa þessu í stað Eining-class-ans og fallsins að neðan.
-	stada = models.SmallIntegerField()
+	# Ný vara sem bætist við lagerinn. Þetta þarf að bæta við stöðuna og
+	# endurstilla við upphaf sölu.
+	nytt = models.SmallIntegerField(null=True, blank=True)
 
-	# Ný vara sem bætist við lagerinn. Þetta þarf að bæta við stöðuna og endurstilla við upphaf sölu.
-	nytt = models.SmallIntegerField(default=0)
-
-	# Lýsing á vörunni og aðrar athugasemdir eins og hvernig hún er að reynast í sölu, hvort kaupa eigi meira af henni eða hætt sé að taka hana inn.
+	# Lýsing á vörunni og aðrar athugasemdir eins og hvernig hún er að
+	# reynast í sölu, hvort kaupa eigi meira af henni eða hætt sé að taka
+	# hana inn.
 	lysing = models.TextField(blank=True)
 	athugasemdir = models.TextField(blank=True)
 
-	def __str__(self):
+	def vorunumer(self):
 		def nullfyllt(tala, saeti):
-			strengur = str(tala)
-			lengd = math.floor(math.log10(tala))
-			for i in range(saeti-lengd-1):
-				strengur = '0'+strengur
-			return strengur
+			if tala < 10:
+				return '0'+str(tala)
+			else:
+				return str(tala)
 
 		return nullfyllt(self.argerd,2)   +'-'+ \
 			   nullfyllt(self.vorugerd,2) +'-'+ \
-			   nullfyllt(self.radnumer,2) +' '+ \
-			   self.stuttnafn + ' (' + str(self.stada) + ')'
+			   nullfyllt(self.radnumer,2)
+
+	def __str__(self):
+		return self.vorunumer() +' '+ self.fulltnafn+ ' (' + str(self.stada) + ')'
+	def verd(self):
+		print('halló')
+		print(Verd.objects.filter(vara=self).latest().smasoluverd)
+		return Verd.objects.filter(vara=self).latest()
 
 	# TODO: Óprófað!!!
-	def stada(self):
-		einingar = Eining.objects.filter(vara=self)
-		return einingar.count()
+	#def stada(self, starfsstod):
+	#	einingar = Eining.objects.filter(vara=self, starfsstod=starfsstod)
+	#	return einingar.count()
+
+	# TODO: Óprófað!!!
+	#def magn(self, starfsstod):
+	#	return 3		
 
 	class Meta:
 		verbose_name_plural = 'vörur'
@@ -57,16 +81,51 @@ class Vara(models.Model):
 # Þetta er bara pæling: Að hafa hverja einingu sér svo hægt sé að gefa henni staðsetningu. Ef við ætlum að vera með bretti eyrnamerkt búðum á milli ára væri ágætt að hafa yfirlit yfir hvar hver vara sé.
 # Þetta gæti líka auðveldað uppgjör búða eftir hvern dag.
 # Við gætum líka breytt þessu í kerfi þar sem gæti tekið við af Trello og þar sem við höfum yfirlit yfir hvar hver vara er á leið sinni frá lager til búðar.
-class Eining(models.Model):
+#class Eining(models.Model):
+#	vara = models.ForeignKey(Vara)
+#	seljast_fyrir = models.SmallIntegerField()
+#	starfsstod = models.ForeignKey(Starfsstod)
+#
+#	class Meta:
+#		verbose_name_plural = 'einingar'
+
+# Eða þetta: Að vera með sölustaðarlagertalningu þannig að hver sölustaður hefur sér fjölda og svo búum við bara til þægilegt viðmót til að færa vöru á milli...
+class Bunki(models.Model):
+	'''
+	Hver bunki inniheldur eina ákveðna vöru á ákveðnum stað eða í flutningi
+	á ákveðnu tæki og gerir kleift að fylgja eftir hversu mikið er til af
+	hverri vöru og hvar hún er.
+	'''
 	vara = models.ForeignKey(Vara)
-	seljast_fyrir = models.SmallIntegerField()
-	stadsetning = models.ForeignKey(Starfsstod)
+	magn = models.SmallIntegerField()
+	
+	# Ef bunkinn er í flutning er starfsstöððin tóm og varan sett á ákvðinn
+	# flutningsaðila. Þá má t.a.m. tilgreina sem A, B, C eða A1, A2, C1.
+	starfsstod = models.ForeignKey(Starfsstod, blank=True)
+
+	# Ártalið sem varan á að seljast á í síðasta lagi.
+	seljast_fyrir = models.SmallIntegerField(blank=True)
+
+	
+	flutningur = models.CharField(max_length=2)
+
+	def __str__(self):
+		return self.vara + str(self.magn)
+
+	class Meta:
+		verbose_name_plural = 'bunkar'
 
 class Verd(models.Model):
-	innkaupsverd = models.SmallIntegerField()
-	smasoluverd = models.SmallIntegerField()
+	'''
+	Verð tilgreinir innkaups- og smásöluverð vöru, og dagsetninguna sem
+	verðið tekur gildi. Verð eru svo tengd vörum svo verðsaga hverrar vöru
+	er geymd.
+	'''
+	innkaupsverd = models.IntegerField(null=True, blank=True)
+	smasoluverd = models.IntegerField(null=True, blank=True)
 	vara = models.ForeignKey(Vara)
 	dags = models.DateField()
 
 	class Meta:
 		verbose_name_plural = 'verð'
+		get_latest_by = 'dags'
