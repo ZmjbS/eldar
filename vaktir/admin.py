47f9cd0d3ea81933ac631ab148da9e3503a12331
Martin Swift <martin@swift.is>
2016-10-31 14:48:31 +0000

    Bætt við dálkum Vaktir í admin-viðmótinu.

--- vaktir/admin.py
+++ vaktir/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from vaktir.models import Timabil,Starfsstod,Tegund,Vakt,Felagi,Skraning
 
+class VaktAdmin(admin.ModelAdmin):
+	list_display = ('starfsstod', 'timabil', 'tegund', 'lagmark', 'hamark', )
+	list_editable = ()
+
 admin.site.register(Timabil)
 admin.site.register(Starfsstod)
 admin.site.register(Tegund)
-admin.site.register(Vakt)
+admin.site.register(Vakt, VaktAdmin)
 admin.site.register(Felagi)
 admin.site.register(Skraning)
