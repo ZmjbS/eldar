2061a7df3f1ac589b1b40b1be8d057b5bd2c40c7
Martin Swift <martin@swift.is>
2016-11-01 01:55:28 +0000

    Mikil endurskoðun og endurskrift til einföldunar.

--- vaktir/views.py
+++ vaktir/views.py
@@ -2,105 +2,60 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from vaktir.models import Vakt, Starfsstod, Timabil, Skraning, Tegund, Felagi
 
-def vaktadagar():
-	""" Búum til lista yfir dagana sem vaktirnar ná yfir og skilum í röðuðum lista.
-	"""
+def dagalisti():
+	''' Búum til lista yfir dagana sem vaktirnar ná yfir og skilum í röðuðum lista.
+	'''
 	dagalisti = []
 	for vakt in Vakt.objects.all():
-		if vakt.dags not in dagalisti:
-			dagalisti.append(vakt.dags)
+		if vakt.timabil.dags() not in dagalisti:
+			dagalisti.append(vakt.timabil.dags())
 	dagalisti.sort()
 
 	return dagalisti
 
-def vaktayfirlit(starfsstod=None):
-	""" Býr til lista yfir daga, tímabil og vaktir.
-		Form tímabilalistans er ólíkt eftir því hvort starfsstöðin er tilgreind.
-
-		  dagar['dagur', 'timabil', timabil_fjoldi']
-		    timabil['timabil', vaktir', 'skraningar', 'lagmark', 'litur',]
-			  vaktir['vakt', 'skraningar', 'lagmark', 'hamark',]
-		    timabil['timabil', vaktin', 'skraningar',]
-	"""
-	dagalisti = vaktadagar()
-	timabilalisti = Timabil.objects.all()
-
-	dagar = []
-	for dagur in dagalisti:
-		# Fyrir hvern dag eru nokkur tímabil...
-		timabil = []
-		for timabilid in timabilalisti:
-			# Finnum vaktirnar og listum upp starfsstöðvarnar
-			vaktir = []
-			if starfsstod is None:
-				for vakt in Vakt.objects.filter(dags=dagur,timabil=timabilid):
-					vaktir.append({
-						'vakt':vakt,
-						'skraningar': len(Skraning.objects.filter(vakt=vakt)),
-						'lagmark': vakt.lagmark,
-						'hamark': vakt.hamark,
-					})
-				# Stingum nú vöktunum og öðrum upplýsingum um tímabilið inn í:
-				timabil.append({
-					'timabil': timabilid,
-					'vaktir': vaktir,
-					'skraningar': timabilid.skraningar(dagur),
-					'lagmark': timabilid.lagmark(dagur),
-					'litur': timabilid.litur(dagur),
-				})
-			else:
-				# Það er bara ein vakt fyrir hvert tímabil á hverri starfsstöð.
-				try:
-					vaktin = Vakt.objects.get(
-						dags=dagur,
-						starfsstod=starfsstod,
-						timabil=timabilid,
-					)
-					# Sækjum allar skráningarnar sem komnar eru.
-					skraningar = Skraning.objects.filter(vakt=vaktin)
-				except:
-					# Ef engin er vaktin setjum við bara inn tóm gildi.
-					vaktin =""
-					skraningar = []
-				# Stingum nú vaktinni og skráningunum inn í 
-				timabil.append({
-					'timabil': timabilid,
-					'vaktin': vaktin,
-					'skraningar': skraningar,
-				})
-		dagar.append({ 'dagur': dagur, 'timabil': timabil, 'timabil_fjoldi': len(timabil), })
-
-	return dagar
-
 def starfsstodvayfirlit():
-	""" Búum til yfirlit yfir vaktir og skráningar á þær.
-	"""
-	dagalisti = vaktadagar()
+	'''
+	Búum til yfirlit yfir vaktir starfsstöðvanna til að senda á yfirlitið.
+	Taflan hefur raðir eftir starfsstöðvum, nema haus hennar inniheldur
+	samantekta mönnun og þörf á mönnun.
+	Hver dálkur inniheldur eitt tímabil og í röðum starfsstaðanna eru .
+	'''
+	#TODO: Klára lýsinguna
 
-	dagar = vaktayfirlit()
+	timabilin = Timabil.objects.all().order_by('hefst')
 
-	starfsstodvar = []
+	starfsstodvalisti = []
+
+	# Förum í gegnum starfsstöðvarnar og búum til lista sem samsvarar tímabilunum í timabil
 	for starfsstod in Starfsstod.objects.all():
-		starfsstodvardagar = vaktayfirlit(starfsstod)
+		vaktir = []
+		for timabilid in timabilin:
+			# Bætum vaktinni við listann ef hún er til:
+			try:
+				vaktir.append(Vakt.objects.get(timabil=timabilid, starfsstod=starfsstod))
+			# Annars fer bara autt stak í listann:
+			except:
+				vaktir.append('')
+		starfsstodvalisti.append({ 'starfsstod': starfsstod, 'vaktir': vaktir, }) 
 
-		# Finnum nú fjölda skráninga og heildarlágmark fyrir hvern starfsstað
-		lagmark = 0
-		skraningar_fjoldi = 0
-		for dagur in starfsstodvardagar:
-			for timabil in dagur['timabil']:
-				if timabil['vaktin'] != '':
-					lagmark = lagmark + timabil['vaktin'].lagmark
-					skraningar_fjoldi = skraningar_fjoldi + len(timabil['skraningar'])
+	# Finnum út hversu mörg tímabil eru á hverjum degi:
+	dagatimabil = []
+	for dagur in dagalisti():
+		fjoldi = Timabil.objects.filter(
+			hefst__day=dagur.day,
+			hefst__month=dagur.month,
+			hefst__year=dagur.year
+			).count()
+		print(fjoldi)
+		dagatimabil.append({ 'dagur': dagur, 'fjoldi_timabila': fjoldi, })
 
-		starfsstodvar.append( { 'starfsstod': starfsstod, 'dagar': starfsstodvardagar, 'skraningar_fjoldi': skraningar_fjoldi, 'lagmark': lagmark, })
-
-	felagalisti = Felagi.objects.all()
-	
 	gogn_til_snidmats = {
-		'starfsstodvar': starfsstodvar,
-		'dagar': dagar,
-		'felagalisti': felagalisti,
-		}
+		'starfsstodvalisti': starfsstodvalisti,
+		'timabilin': timabilin,
+		'dagar': dagalisti(),
+		'dagatimabil': dagatimabil,
+	}
+
 	return gogn_til_snidmats
 
 def yfirlit(request):
@@ -109,11 +64,14 @@ def yfirlit(request):
 	return render_to_response('vaktir/yfirlit.html', starfsstodvayfirlit() )
 
 def skraning(request):
-	""" Skilar viðmóti sem býður notanda upp á að skrá sig. Til dæmis svæði fyrir kennitölu og lág tafla yfir vaktir á tímanakkkk
 	"""
-	dagar = vaktayfirlit()
+	Skilar viðmóti sem býður notanda upp á að skrá sig. Til dæmis svæði
+	fyrir kennitölu og lág tafla yfir vaktir á tímana.
+	"""
 
-	return render(request, 'vaktir/skraning.html', { 'dagar': dagar, } )
+	gogn_til_snidmats = starfsstodvayfirlit()
+
+	return render(request, 'vaktir/skraning.html', starfsstodvayfirlit() )
 
 def skra(request):
 	""" Tekur við POST beiðni, vistar skráninguna og skilar upplýsingum til notanda um hana.
@@ -139,24 +97,7 @@ def skra(request):
 
 def smidi(request):
 
-	starfsstodvarlisti = Starfsstod.objects.all()
-	dagalisti = []
-	for vakt in Vakt.objects.all():
-		if vakt.dags not in dagalisti:
-			dagalisti.append(vakt.dags)
-	dagalisti.sort()
-	timabilalisti = Timabil.objects.all()
+	gogn_til_snidmats = starfsstodvayfirlit()
+	gogn_til_snidmats['tegundalisti'] = Tegund.objects.all()
 
-	tegundalisti = Tegund.objects.all()
-
-	# Hérna væri gott að taka út þær starfsstöðvar sem þegar er búið að skrá vaktir á, eða möndla það þannig að við fá... ah, hvað með að búa bara til form á vaktalausu dagana? ... ah, nei, við viljum náttúrulega líka geta breytt hinum.
-	# Já, kannski er bara best að vera með alla púllíuna/töfluna og bæta við edit-pakka á þessu view-i...
-
-	gogn_til_snidmats = {
-		'dagalisti': dagalisti,
-		'timabilalisti': timabilalisti,
-		#'starfsstodvar': starfsstodvar,
-		'starfsstodvarlisti': starfsstodvarlisti,
-		'tegundalisti': tegundalisti,
-		}
-	return render_to_response('vaktir/smidi.html', gogn_til_snidmats)
+	return render(request, 'vaktir/skraning.html', gogn_til_snidmats)
