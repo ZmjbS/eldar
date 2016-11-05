cf9072f3de9eeb3ff8c0405b60b0dcc178b9f9f2
Martin Swift <martin@swift.is>
2016-11-05 00:09:44 +0000

    Skráningin sett á grunn-veffangið og gamalt dót óvirkjað.

--- eldar/urls.py
+++ eldar/urls.py
@@ -7,10 +7,12 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
      #url(r'^$', 'eldar.views.home', name='home'),
-     url(r'^vaktir$', 'vaktir.views.yfirlit', name='yfirlit'),
-     url(r'^vaktir/smidi$', 'vaktir.views.smidi', name='smidi'),
-     url(r'^vaktir/skraning$', 'vaktir.views.skraning', name='skraning'),
+     #url(r'^vaktir$', 'vaktir.views.yfirlit', name='yfirlit'),
+     url(r'^vaktir$', 'vaktir.views.skraning', name='skraning'),
      url(r'^vaktir/skra$', 'vaktir.views.skra', name='skra'),
+     url(r'^vaktir/umsjon$', 'vaktir.views.umsjon', name='umsjon'),
+     #url(r'^vaktir/smidi$', 'vaktir.views.smidi', name='smidi'),
+     #url(r'^vaktir/skraning$', 'vaktir.views.skraning', name='skraning'),
      #url(r'^vaktir/umsjon$', 'vaktir.views.umsjon', name='yfirlit'),
 
      url(r'^lager$', 'lager.views.yfirlit', name='yfirlit'),
