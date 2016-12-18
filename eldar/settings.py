c0666969848b4b67ca4c85776686139792080cad
Baldur Árnason <baldurarna@meniga.com>
2016-12-18 00:13:13 +0000

    no message

--- eldar/settings.py
+++ eldar/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'eldar.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
 }
