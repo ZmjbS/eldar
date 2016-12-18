d60b7881263226b4549446ac60453dd42fb0c982
Baldur Árnason <baldurarna@meniga.com>
2016-12-18 00:19:58 +0000

    no message

--- eldar/settings.py
+++ eldar/settings.py
@@ -80,7 +80,12 @@ WSGI_APPLICATION = 'eldar.wsgi.application'
 # # Database
 # # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
+}
 
 # This needs further investigation
 CACHES = {
