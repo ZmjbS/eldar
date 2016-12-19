bff327ca5ef927ae08e234a56ca2fb536adc9196
Baldur Árnason <baldurarna@meniga.com>
2016-12-19 00:46:03 +0000

    no message

--- frontend/src/api/baseApi.js
+++ frontend/src/api/baseApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 console.log('BASE URL', window.apiUrl);
 const instance = axios.create({
- baseURL: 'https://eldar.herokuapp.com/api',
- // baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://eldar.herokuapp.com/api',
+ //baseURL: 'http://localhost:8000/api',
   //timeout: 4000
 });
 
